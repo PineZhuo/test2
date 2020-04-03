592a00ceec0a5df35d9c18f25f93bcaa024b2c86
PineZhuo <42902711+PineZhuo@users.noreply.github.com>
2020-04-03 21:54:48 +0800

    Create index.py

---
+++ index.py
@@ -0,0 +1,13 @@
+# class MyClass1:
+#     i = 123
+#     def func(self):
+#         return "Hello World!"
+# x = MyClass1()
+# print(x.i)
+# print(x.func())
+# class MyClass2:
+#     def __init__(self, realpart, imagpart):
+#         self.r = realpart
+#         self.i = imagpart
+# x = MyClass2(3.0, -4.5)
+# print(x.i, x.r)
